ade51476a1f7a6060a5a19aae2462cb7f0daa614
IgorsK <igors.krasjukovs@wearedots.com>
2020-09-25 17:05:53 +0300

    Added unit-test projects

---
+++ Onion.Application.Tests/Exceptions/AccountExceptionTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using Onion.Application.Exceptions;
+using Xunit;
+using FluentAssertions;
+
+namespace Onion.Application.Tests.Exceptions
+{
+    public partial class AccountExceptionTests
+    {
+        [Fact]
+        public void AccountException_Constructor_WithSerializableProperties()
+        {
+            // Arrange
+            AccountException ex = new AccountException("Message", "Account");
+            AccountException deserializedEx = null;
+
+            // Assert
+            ex.Message.Should().Be("Message");
+            ex.Account.Should().Be("Account");
+
+            // Act
+            string exception = ex.ToString();
+
+            // Round-trip the exception: Serialize and de-serialize with a BinaryFormatter
+            BinaryFormatter bf = new BinaryFormatter();
+            using (MemoryStream ms = new MemoryStream())
+            {
+                // "Save" object state
+                bf.Serialize(ms, ex);
+
+                // Re-use the same stream for de-serialization
+                ms.Seek(0, 0);
+
+                // Fill exception with de-serialized one
+                deserializedEx = (AccountException)bf.Deserialize(ms);
+            }
+
+            // Assert
+            deserializedEx.Message.Should().Be("Message");
+            deserializedEx.Account.Should().Be("Account");
+            ex.ToString().Should().Be(deserializedEx.ToString());
+        }
+    }
+}
